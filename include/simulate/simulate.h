e99852b11bb138baed72fa4319188a27339eb55d
Bernhard Paus Græsdal <bernhardpg@gmail.com>
2020-05-08 13:03:03 +0200

    Add office environment with crash detection

--- include/simulate/simulate.h
+++ include/simulate/simulate.h
@@ -2,12 +2,18 @@
 
 #include <iostream>
 
-#include "drake/systems/framework/diagram_builder.h"
-#include "drake/geometry/geometry_visualization.h"
-#include "drake/systems/analysis/simulator.h"
-#include <drake/systems/primitives/signal_logger.h>
+#include <gflags/gflags.h>
 
+#include "drake/common/drake_assert.h"
+#include "drake/common/find_resource.h"
 #include "drake/common/is_approx_equal_abstol.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/primitives/signal_logger.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/multibody/plant/contact_results_to_lcm.h"
+#include "drake/geometry/geometry_visualization.h"
 
 #include "drake/examples/quadrotor/quadrotor_plant.h"
 #include "drake/examples/quadrotor/quadrotor_geometry.h"
--- src/simulate/simulate.cpp
+++ src/simulate/simulate.cpp
@@ -1,5 +1,11 @@
 #include "simulate/simulate.h"
 
+DEFINE_double(target_realtime_rate, 1.0,
+              "Rate at which to run the simulation, relative to realtime");
+DEFINE_double(simulation_time, 10, "How long to simulate the pendulum");
+DEFINE_double(max_time_step, 1.0e-3,
+              "Simulation time step used for integrator.");
+
 void simulate()
 {
 	std::cout << "Running drake simulation" << std::endl;
@@ -15,17 +21,90 @@ void simulate()
 	double m = 2.856;
 	double arm_length = 0.2;
 
-	// ********
-	// Set up quadrotor system
-	// ********
+	// **********
+	// Build model diagram 
+	// **********
+	
+	DRAKE_DEMAND(FLAGS_simulation_time > 0);
 
+	// Setup diagram
 	drake::systems::DiagramBuilder<double> builder;
+	auto [plant, scene_graph] =
+        drake::multibody::AddMultibodyPlantSceneGraph(&builder, 0.0);
 
-	auto quadrotor = builder
+	// Add obstacles from file
+	drake::multibody::Parser parser(&plant, &scene_graph);
+	auto obstacles = parser.AddModelFromFile(
+        drake::FindResourceOrThrow("drake/examples/quadrotor/office.urdf"));
+	plant.WeldFrames(
+			plant.world_frame(), plant.GetFrameByName("wall1", obstacles));
+
+	// Load quadrotor model
+	auto quadrotor_plant = builder
 		.AddSystem<drake::examples::quadrotor::QuadrotorPlant<double>>(
 				m, arm_length, inertia, 1, 0.0245
 				);
-	quadrotor->set_name("quadrotor");
+	quadrotor_plant->set_name("quadrotor");
+	// Add quadrotor geometry
+	drake::examples::quadrotor::QuadrotorGeometry::AddToBuilder(
+      &builder, quadrotor_plant->get_output_port(0), &scene_graph);
+
+	// Create LQR controller
+  const Eigen::Vector3d kNominalPosition{((Eigen::Vector3d() << 3.0, -2.0, 1.0).
+      finished())};
+	auto lqr_controller = builder.AddSystem(
+			drake::examples::quadrotor::StabilizingLQRController(quadrotor_plant, kNominalPosition)
+			);
+  lqr_controller->set_name("lqr_controller");
+
+	// Connect controller and quadrotor
+	builder.Connect(quadrotor_plant->get_output_port(0), lqr_controller->get_input_port());
+  builder.Connect(lqr_controller->get_output_port(), quadrotor_plant->get_input_port(0));
+
+	plant.Finalize();
+	// Connect to 3D visualization 
+	drake::geometry::ConnectDrakeVisualizer(&builder, scene_graph);
+	drake::multibody::ConnectContactResultsToDrakeVisualizer(&builder, plant);
+
+	auto diagram = builder.Build();
+
+
+	// ********
+	// Simulate
+	// ********
+	
+
+
+	drake::systems::Simulator<double> simulator(*diagram);
+
+	// Initial conditions
+	Eigen::VectorX<double> x0 = Eigen::VectorX<double>::Zero(12);
+	//x0 = Eigen::VectorX<double>::Random(12);
+	x0 << 0,1,2,
+				0,0,0,
+				0,0,0,
+				0,0,0;
+
+	// To set initial values for the simulation:
+	// * Get the Diagram's context.
+	// * Get the part of the Diagram's context associated with particle_plant.
+	// * Get the part of the particle_plant's context associated with state.
+	// * Fill the state with initial values.
+	drake::systems::Context<double>& simulator_context = simulator.get_mutable_context();
+	drake::systems::Context<double>& quadrotor_plant_context =
+			diagram->GetMutableSubsystemContext(*quadrotor_plant, &simulator_context);
+
+	quadrotor_plant_context.get_mutable_continuous_state_vector().SetFromVector(x0);
+
+	simulator.Initialize();
+	simulator.set_target_realtime_rate(FLAGS_target_realtime_rate);
+	simulator.AdvanceTo(FLAGS_simulation_time); // seconds
+
+/*
+
+
+	auto diagram = builder.Build();*/
+
 
 	//auto logger = builder.AddSystem<drake::systems::SignalLogger<double>>(4);
 
@@ -34,38 +113,11 @@ void simulate()
 	//builder.Connect(controller->get_output_port(), logger->get_input_port());
 
 
-
-	// // TODO REMOVE
-	// Set up LQR controller for test
-  const Eigen::Vector3d kNominalPosition{((Eigen::Vector3d() << 0.0, 0.0, 1.0).
-      finished())};
-
-	auto lqr_controller = builder.AddSystem(
-			drake::examples::quadrotor::StabilizingLQRController(quadrotor, kNominalPosition)
-			);
-  lqr_controller->set_name("lqr_controller");
-
-	// Connect controller and quadrotor
-	builder.Connect(quadrotor->get_output_port(0), lqr_controller->get_input_port());
-  builder.Connect(lqr_controller->get_output_port(), quadrotor->get_input_port(0));
-
-	// *********
-	// Set up visualization
-	// *********
-
-  auto scene_graph = builder.AddSystem<drake::geometry::SceneGraph>();
-	drake::examples::quadrotor::QuadrotorGeometry::AddToBuilder(
-      &builder, quadrotor->get_output_port(0), scene_graph);
-	drake::geometry::ConnectDrakeVisualizer(&builder, *scene_graph);
-
-	auto diagram = builder.Build();
-
-
 	// *********
 	// Simulate
 	// *********
 
-	// Initial conditions
+	/*// Initial conditions
 	Eigen::VectorX<double> x0 = Eigen::VectorX<double>::Zero(12);
 	x0 = Eigen::VectorX<double>::Random(12);
 
@@ -87,5 +139,6 @@ void simulate()
 	const drake::systems::Context<double>& context = simulator.get_context();
 	const drake::systems::ContinuousState<double>& state = context.get_continuous_state();
 	const Eigen::VectorX<double>& position_vector = state.CopyToVector();
+	*/
 
 }
